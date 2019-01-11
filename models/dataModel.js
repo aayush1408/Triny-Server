157c60247947ae0ae57ee60f568cddf5231745b3
aayush1408 <aayushjaiswal0114@gmail.com>
2019-01-11 15:49:27 +0530

    schema altered

--- models/dataModel.js
+++ models/dataModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const dataSchema = new Schema({
-  name: String,
-  age: Number
-});
+}, {
+    strict: false
+  });
 
 const dataModel = mongoose.model('dataModel', dataSchema);
 
